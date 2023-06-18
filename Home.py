14c9c06b5fb92567b40608dee4509800c0822bbe
Stephan Nef <65945515+nefste@users.noreply.github.com>
2023-06-19 01:11:48 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,17 +1,26 @@
 import streamlit as st
+from streamlit_lottie import st_lottie
+import base64
 
 st.set_page_config(
-    page_title="NLP Project - Lukas and Stephan",
-    page_icon="🐝",
-    layout="wide",
+     page_title="NLP Project - Lukas and Stephan",
+     page_icon="🐝",
+     layout="wide",
 )
 
-
-# Main Streamlit App
 def main():
     st.title("Uncovering the Authors")
     st.subheader("A Dual Approach to Detect and Understand Authorship in Text Documents")
-
-
+    st.write("Navigate in the Sidebar through our different Pages to get some interessting insights in some of our analytics we have done for fingerprint analysis and authorship change detection.")
+    st_lottie("https://assets9.lottiefiles.com/packages/lf20_bUYZYN2sM0.json", height=200)
+    
+    with open("NLP_Report.pdf", "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.subheader("Project Report")
+    st.markdown(pdf_display, unsafe_allow_html=True)
+    
+    
 if __name__ == '__main__':
     main()
